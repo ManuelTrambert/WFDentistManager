83f497b6e118cdde080e29b1d3318bf22a6a6546
Thomas Gillard <thomas.gillard@epitech.eu>
2018-03-28 21:59:36 +0000

    Save the prod 3: The return

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
+  get 'users/new'
+
   root 'static_pages#home'
   get  '/about',   to: 'static_pages#about'
   get  '/signup',  to: 'users#new'
