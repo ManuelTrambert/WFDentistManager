7072c8d59aa5a6626d8a83ea482725e0e3b9f534
Thomas Gillard <thomas.gillard@epitech.eu>
2018-03-28 22:10:34 +0000

    try to admin mode

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -49,7 +49,7 @@ class UsersController < ApplicationController
   private
 
     def user_params
-      params.require(:user).permit(:first_name, :last_name, :email, :password,
+      params.require(:user).permit(:first_name, :last_name, :email, :admin, :password,
                                    :password_confirmation)
     end
 
