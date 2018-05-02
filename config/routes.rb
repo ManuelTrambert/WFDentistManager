1a1d000b1867b2f0002512f0e034565f8dccd611
ManuelTrambert <m.trambert@gmail.com>
2018-05-02 01:52:17 +0100

    test user show

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
-  post '/edit', to: 'users#update'
+  post '/edit/:id', to: 'users#update'
 
   resources :users
   resources :account_activations, only: [:edit]
