719dab41cdbfb7d79792838c46a6f8bc401db790
ManuelTrambert <m.trambert@gmail.com>
2018-05-02 03:55:53 +0100

    admin authorization

---
+++ app/controllers/admin_controller.rb
@@ -0,0 +1,25 @@
+class AdminController < ApplicationController
+  before_filter :authorized?
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  private
+  def authorized?
+    unless current_user.has_role? :admin
+      flash[:error] = "You are not authorized to view that page."
+      redirect_to root_path
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,10 @@ Rails.application.routes.draw do
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
 
+  namespace :admin do
+    resources :users
+  end
+
   resources :users
   resources :account_activations, only: [:edit]
   resources :password_resets,     only: [:new, :create, :edit, :update]
