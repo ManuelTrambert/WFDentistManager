00700301953d5b8473997cbc976519eb9e672bc9
ManuelTrambert <m.trambert@gmail.com>
2018-05-03 01:05:03 +0100

    gem install

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ApplicationRecord
             uniqueness: {case_sensitive: false}
   has_secure_password
   validates :password, presence: true, length: {minimum: 6}, allow_nil: true
-  has_one :staff
+  has_many :staff
   class << self
     def digest(string)
       cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
