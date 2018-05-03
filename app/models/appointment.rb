d86020dcaeb6b673aa2784bdc824ccc9f8b74562
ManuelTrambert <m.trambert@gmail.com>
2018-05-03 03:39:21 +0100

    form

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,6 +1,6 @@
 class Appointment < ApplicationRecord
-  belongs_to :staff
-  belongs_to :patient
+  belongs_to :user, through: :staff
+  belongs_to :user, through: :patient
   validates :staff_id, presence: true
   validates :patient_id, presence: true
 end
