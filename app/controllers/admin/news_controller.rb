4ca828f1bb05e3c4788868305d95d616fc527c0a
ManuelTrambert <m.trambert@gmail.com>
2018-05-13 21:25:06 +0100

    fix

--- app/controllers/admin/news_controller.rb
+++ app/controllers/admin/news_controller.rb
@@ -8,7 +8,7 @@ class Admin::NewsController < AdminController
     @news = News.find(params[:id])
     if @news.update_attributes(news_params)
       flash[:success] = "News updated"
-      redirect_to @news
+      redirect_to root_url
     else
       render 'edit'
     end
